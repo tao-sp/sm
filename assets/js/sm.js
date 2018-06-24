f7e0cfb946df8989bf4117a96a2859264f522b4f
陶 <382803687@qq.com>
2018-06-24 13:52:47 +0800

    20180624

--- assets/js/sm.js
+++ assets/js/sm.js
@@ -23,6 +23,29 @@ $(function () {
                return false;
            });
        });
+    /*关于我们发展历程*/
+    (function() {
+        /*
+        SuperSlide组合注意：
+        1、内外层mainCell、targetCell、prevCell、nextCell等对象不能相同，除非特殊应用；
+        2、注意书写顺序，通常先写内层js调用，再写外层js调用
+        */
+        /* 发展历程内层图片滚动切换 */
+        jQuery(".ab-development .slideGroup .slideBox").slide({ mainCell:"ul",vis:5,prevCell:".sPrev",nextCell:".sNext",effect:"leftLoop"});
+
+        /* 发展历程外层tab切换 */
+        jQuery(".ab-development .slideGroup").slide({titCell:".parHd li",mainCell:".parBd"});
+
+    })();
+
+    /*关于我们发展历程*/
+    (function() {
+        var oDevelop = $('#ab-development'),
+            oList = oDevelop.find('.parHd .tab-list li');
+        oList.on('mouseenter',function() {
+            $(this).addClass('on').siblings().removeClass('on');
+        });
+    })();
 })
 
 
